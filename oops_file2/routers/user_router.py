449dd0a87d8f4ec8f8def4d8bfbec886a7c508f4
Tejaswi Basavaraj Kiranagi <tejaswikiranagi123@gmail.com>
2026-03-12 12:28:48 +0530

    Delete oops_file2/routers directory

--- oops_file2/routers/user_router.py
+++
@@ -1,8 +0,0 @@
-# from oops_file2.services.notification_service import UserService
-
-# class UserRouter:
-
-#     def create(self, name, email):
-#         service = UserService()
-#         user = service.create_user(name, email)
-#         return user
